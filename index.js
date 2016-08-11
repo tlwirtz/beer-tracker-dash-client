86b87fb2598bb7a3b6a48a1e43c0d78ea8c16680
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-08-11 13:18:43 -0700

    skeleton of client now set up

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const sniffer = require('./lib/sniffer');
+const client = require('./lib/dash-client');
+
+const dashes = [
+  {
+    macAddr: '7475483d7f6a',
+    name: 'gatorade'
+  },
+  {
+    macAddr: '84d6d03e535f',
+    name: 'smart water;'
+  },
+  {
+    macAddr: '44650dd5ef23',
+    name: 'red bull'
+  }
+];
+
+const dashClient = client.startClient(dashes);
+sniffer.sniffForDevice(dashClient);
