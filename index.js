44941c602f299616c62050e60184a30cd42c334e
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-09-03 14:50:24 -0700

    basic communication with API up and running

--- index.js
+++ index.js
@@ -2,27 +2,30 @@
 
 const sniffer = require('./lib/sniffer');
 const client = require('./lib/dash-client');
-const debug = require('debug')('dashClient');
 const beerTracker = require('./lib/beer-tracker')(process.env.BEER_TRACKER_SERVER);
 
 const dashes = [
   {
-    macAddr: '7475483d7f6a',
+    macId: '7475483d7f6a',
     name: 'gatorade'
   },
   {
-    macAddr: '84d6d03e535f',
+    macId: '84d6d03e535f',
     name: 'smart water;'
   },
   {
-    macAddr: '44650dd5ef23',
+    macId: '44650dd5ef23',
     name: 'red bull'
   }
 ];
 
 
 beerTracker.getDevices()
-.then(devices => debug(devices));
-
-const dashClient = client.startClient(dashes);
-sniffer.sniffForDevice(dashClient);
+.then(devices => {
+  if (devices.length === 0) return dashes;
+  return devices;
+})
+.then(clientDevices => {
+  const dashClient = client.startClient(clientDevices);
+  sniffer.sniffForDevice(dashClient);
+});
